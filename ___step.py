10a2f55af92128b8360373790542fb8376c3ce61
ruabsarnul <122334016+ruabsarnul@users.noreply.github.com>
2023-03-14 22:02:20 +0600

    updated

--- ___step.py
+++ ___step.py
@@ -1,6 +1,6 @@
 '''
 --find the index position of the alpabet
--- module the result here >>> 200 182, 180 156, 240 234, 60 52, 80 78, 2
+-- modulus the result here >>> 200 182, 180 156, 240 234, 60 52, 80 78, 2
 
 
 
